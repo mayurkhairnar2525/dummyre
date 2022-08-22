c95c4798fa8227c5301a9e08fd653ca803572f76
mayurkhairnar2525 <mayurkhairnar325@gmail.com>
2022-08-22 10:59:52 +0530

    adding sorting algo

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
-import "fmt"
+// Sorting algo
 
-func hello() {
-	fmt.Println("hello world")
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
 }
 
-func sayHello(msg string) {
-	fmt.Println(msg)
+func insertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[i] {
+				arr[j], arr[i] = arr[i], arr[j]
+			}
+		}
+	}
+}
+
+func selectionSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		midIndex := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[midIndex] {
+				arr[j], arr[midIndex] = arr[midIndex], arr[j]
+			}
+		}
+	}
 }
 
 func main() {
-	hello()
-	sayHello("hello world")
+
 }
