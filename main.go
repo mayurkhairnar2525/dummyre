a2efc606afb4e8f6041943530569fa463d7142f1
mayurkhairnar2525 <mayurkhairnar325@gmail.com>
2022-08-27 13:03:18 +0530

    adding

--- main.go
+++ main.go
@@ -2,15 +2,47 @@ package main
 
 import "fmt"
 
-func git() {
-	fmt.Println("Hello, From git")
+// Sorting algo
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
 }
 
-func sayHello(msg string) {
-	fmt.Println(msg)
+func insertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[i] {
+				arr[j], arr[i] = arr[i], arr[j]
+			}
+		}
+	}
+}
+
+func selectionSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		midIndex := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[midIndex] {
+				arr[j], arr[midIndex] = arr[midIndex], arr[j]
+			}
+		}
+	}
 }
 
 func main() {
-	git()
-	sayHello("hi")
+	arr := []int{50, 10, 40, 30, 20, 00}
+	bubbleSort(arr)
+	fmt.Println(arr)
+
+	insertionSort(arr)
+	fmt.Println(arr)
+
+	selectionSort(arr)
+	fmt.Println(arr)
 }
